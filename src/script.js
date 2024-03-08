7ac83ee8461ecae0e1d219366efe613e7ebd7c8d
jorkam2000 <jorkam2000@gmail.com>
2024-03-08 20:50:07 +0000

    Estructura y procedimiento de envío del formulario

---
+++ src/script.js
@@ -0,0 +1,25 @@
+function enviarFormulario(event) {
+  const form = document.getElementById("contacto");
+  for (var i = 0; i < form.elements.length; i++) {
+    if (
+      form.elements[i].value === "" &&
+      form.elements[i].hasAttribute("required")
+    ) {
+      alert("Debe rellenar todos los campos obligatorios!");
+      return false;
+    }
+  }
+
+  if (document.getElementById("politicaPrivacidad").checked == false) {
+    alert(
+      "Debe aceptar la politica de privacidad antes de enviar el formulario"
+    );
+  } else {
+    alert("Formulario enviado!");
+  }
+}
+
+window.onload = function () {
+  let botonEnviar = document.getElementById("botonEnviar");
+  botonEnviar.addEventListener("click", enviarFormulario);
+};
